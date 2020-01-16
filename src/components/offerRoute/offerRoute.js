61b4b46d5d7a998b3ffdcb478ecfd62aa077b878
David <davidbalayan1@gmail.com>
2020-01-16 23:39:28 +0400

    errors validation in offer rpute

--- src/components/offerRoute/offerRoute.js
+++ src/components/offerRoute/offerRoute.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, {useState}from 'react';
 import {classes} from "./style";
-import {TextField, Table, TableRow, TableHead, TableBody, TableCell, TableFooter, TablePagination} from '@material-ui/core';
+import {TextField, Table, TableRow, TableHead, TableBody, TableCell, TableFooter, TablePagination, Button} from '@material-ui/core';
 // import TablePaginationActions from "@material-ui/core/TablePagination/TablePaginationActions";
 
 const OfferRout = () => {
+    // const [from,setFrom] = useState("");
+    // const [to,setTo] = useState("");
+    // const [startDate,setStartDate] = useState("");
+    // const [maxPersons, setMaxPersons] = useState("");
+    // const [carModel, setCarModel] = useState("");
+    // const [carPlate, setCarPlate] = useState("");
+    const [errors, setErrors] = useState({
+        from: false,
+        to: false,
+        startDate: false,
+        maxPersons: false,
+        carModel: false,
+        carPlate: false,
+    })
+    const [state,setState] = useState({
+        from: "",
+        to: "",
+        startDate: "",
+        maxPersons: "",
+        carModel: "",
+        carPlate: "",
+    })
+
+   function onSubmitClick(){
+        let keys=Object.entries(state);
+
+       keys.forEach(element => {
+        if(element[1]) {
+            let errorF={};
+            errorF[element[0]] = false;
+            setErrors(Object.assign(errors, errorF))
+           }else{
+            let errorT = {};
+            errorT[element[0]] = true;
+            console.log(errorT)
+            setErrors(Object.assign(errors, errorT))
+           }    
+
+    });
+    console.log(errors)
+    // console.log(state);
+   }
+
     let d = new Date();
     let day = d.getDate();
     let month;
@@ -22,12 +65,15 @@ const OfferRout = () => {
                     margin='dense'
                     variant='outlined'
                     label='From'
+                    onChange={(e)=>{setState({from: e.target.value})}}
+                    error = {errors.from}
                     style={classes.routeListItem}
                 />
                 <TextField
                     margin='dense'
                     variant='outlined'
                     label='To'
+                    onChange={(e)=>{setState({to: e.target.value})}}
                     style={classes.routeListItem}
                 />
                 <TextField
@@ -35,6 +81,7 @@ const OfferRout = () => {
                     variant='outlined'
                     label='Date'
                     type='datetime-local'
+                    onChange={(e)=>{setState({startDate: e.target.value})}}
                     defaultValue={`${date}`}
                     style={classes.routeListItem}
                 />
@@ -42,20 +89,24 @@ const OfferRout = () => {
                     margin='dense'
                     variant='outlined'
                     label='Persons'
+                    onChange={(e)=>{setState({maxPersons: e.target.value})}}
                     style={classes.routeListItem}
                 />
                 <TextField
                     margin='dense'
                     variant='outlined'
                     label='Car Model'
+                    onChange={(e)=>{setState({carModel: e.target.value})}}
                     style={classes.routeListItem}
                 />
                 <TextField
                     margin='dense'
                     variant='outlined'
                     label='Car plate'
+                    onChange={(e)=>{setState({carPlate: e.target.value})}}
                     style={classes.routeListItem}
                 />
+                <Button onClick={onSubmitClick}>Submit</Button>
             </div>
         </section>
     );
