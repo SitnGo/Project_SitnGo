b18faf0028a9ad0c4ff14dfb9b9cebfd2bff21ef
VGhazaryan <49144300+VGhazaryan@users.noreply.github.com>
2020-01-26 18:12:40 +0400

    1

--- src/components/profilePage/personalInfo.js
+++ src/components/profilePage/personalInfo.js
@@ -1,7 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import {Typography, Button, Avatar, Paper, Grid} from '@material-ui/core';
-import EditIcon from '@material-ui/icons/Edit';
-import {Phone, Email, AccountBox } from '@material-ui/icons'
+import {Phone, Email, AccountBox, Edit } from '@material-ui/icons'
 import Skeleton from '@material-ui/lab/Skeleton';
 import UpdateForm from './userUpdateForm/userUpdateForm';
 import ConfirmPassword from './ConfirmPassword/confirmPassword';
@@ -15,15 +14,21 @@ import CenteredTabs from './TabPanels/tabPanels';
 import {useDispatch, useSelector, connect} from 'react-redux';
 import styles from './style';
 
+function mapStateToProps(state) {
+    return {
+        user: state.user,
+        isLoggedInUser: state.isLoggedInUser,
+        isEdit1:state.isEdit1,
+    };
+}
+
 function usePersonalInfo() {
-   
     const [PassagerList, setPassagerList ] = useState(null);
     const [DriverList, setDriverList] = useState(null);
     const [bool, changeBool] = useState(false);
     const [tabChange, setTabChange] = useState(false);
     const [user, setUser] = useState({});
     const [url, setUrl] = useState('');
-
     const classes = styles();
     const dispatch = useDispatch();
     let update = useSelector(state => state.confirmUpdate);
@@ -36,10 +41,8 @@ function usePersonalInfo() {
             let userId = fire.auth().currentUser.uid;
             user = await fire.firestore().collection('users').doc(userId).get()
             user = user.data();
-
             if(user && user.userRoutesInfo && user.userRoutesInfo.routes){
-
-                setDriverList(user.userRoutesInfo.routes)
+                setDriverList(user.userRoutesInfo.routes);
             }
             if(user && user.acceptedRoutes && user.acceptedRoutes.length){
                 setPassagerList(user.acceptedRoutes);
@@ -58,67 +61,107 @@ function usePersonalInfo() {
         });
     },[update, user1]);
 
-
-
     function isEditBtnClick() {
         dispatch(isEdit1());
     }
 
     return(
-        <Grid container sm={12}  className={classes.profileContainer}>
-            {console.log("rerendered")}
-            
-            <Grid item sm={4} xs={12} className={classes.leftSide}>
-                <Paper elevation={3} className={classes.personalInfoBlock1}>
-
-                    <div className={classes.header2}>
-                        {   isEdit ? (<Avatar className={classes.bigAvatar} src={bool ? url : ''}></Avatar>)
+        <Grid
+            container
+            xs={12}
+            className={classes.profileContainer}
+        >
+            <Grid
+                item
+                sm={4}
+                xs={12}
+            >
+                <Paper
+                    elevation={3}
+                    className={classes.personalInfoBlock1}
+                >
+                    <div>
+                        {   isEdit ?
+                                <Avatar
+                                    src={bool ? url : ''}
+                                    variant='circle'
+                                    className={classes.bigAvatar}
+                                ></Avatar>
                             :
-                            <DropzoneDialog url={url} setUrl={setUrl} />
+                                <DropzoneDialog url={url} setUrl={setUrl} />
                         }
-
                     </div>
                     <hr/>
-                    {isEdit ? (
-                        <FadeIn>   
-                        <Paper className={classes.paper} elevation={3}>         
-                            <AccountBox fontSize="large"/>
-                            {bool ? <Typography className={classes.typography}>  {user && user.userInfo.name} </Typography>:<Skeleton height={60} component='p'/>}
-                        </Paper>
-                        <Paper className={classes.paper} elevation={3}>
-                            <AccountBox fontSize="large"/>
-                            {bool ? <Typography className={classes.typography}>  {user && user.userInfo.surname}</Typography>:<Skeleton height={60} component='p'/> }
-                        </Paper>
-                        <Paper className={classes.paper} elevation={3}>
-                            <Phone fontSize="large"/>
-                            {bool ? <Typography className={classes.typography}>  {user && user.userInfo.phone}</Typography>:<Skeleton height={60} component='p'/> } 
-                        </Paper>
-                        <Paper className={classes.paper} elevation={3}>    
-                            <Email fontSize="large"/>
-                            {bool ? <Typography className={classes.typography}> {user && user.userInfo.email}</Typography>:<Skeleton height={60} component='p'/>}
-                        </Paper>   
+                    {
+                        isEdit ? (
+                            <FadeIn>
+                                <Paper className={classes.paper} elevation={3}>
+                                    <AccountBox fontSize="large"/>
+                                    {
+                                        bool ?
+                                            <Typography className={classes.typography}>
+                                                {user && user.userInfo.name}
+                                            </Typography>
+                                        :
+                                            <Skeleton height={60} component='p'/>
+                                    }
+                                </Paper>
+                                <Paper className={classes.paper} elevation={3}>
+                                    <AccountBox fontSize="large"/>
+                                    {
+                                        bool ?
+                                            <Typography className={classes.typography}>
+                                                {user && user.userInfo.surname}
+                                            </Typography>
+                                        :
+                                            <Skeleton height={60} component='p'/>
+                                    }
+                                </Paper>
+                                <Paper className={classes.paper} elevation={3}>
+                                    <Phone fontSize="large"/>
+                                    {
+                                        bool ?
+                                            <Typography className={classes.typography}>
+                                                {user && user.userInfo.phone}
+                                            </Typography>
+                                        :
+                                            <Skeleton height={60} component='p'/>
+                                    }
+                                </Paper>
+                                <Paper className={classes.paper} elevation={3}>
+                                    <Email fontSize="large"/>
+                                    {
+                                        bool ?
+                                            <Typography className={classes.typography}>
+                                                {user && user.userInfo.email}
+                                            </Typography>
+                                        :
+                                            <Skeleton height={60} component='p'/>
+                                    }
+                                </Paper>
                             </FadeIn>
-
                         ) : (
                             <>
-                                
                                 {!openUpdateFormBool ? (
                                     <>
-                                        <UpdateForm  data={[user.userInfo.email, user.userInfo.phone]} userId={fire.auth().currentUser.uid}/>                          
-                                </>
-                             ) : <ConfirmPassword />} 
-                        </>
-                    )}
-
+                                        <UpdateForm
+                                            data={[user.userInfo.email, user.userInfo.phone]}
+                                            userId={fire.auth().currentUser.uid}
+                                        />
+                                    </>
+                                ) : <ConfirmPassword />}
+                            </>
+                        )
+                    }
                     <Button
                         className={isEdit ? classes.editButton : classes.hideEditButton}
                         disabled={bool ? false : true}
                         variant='contained'
                         color='secondary'
-                        onClick={isEditBtnClick}>
-                        <EditIcon/>Edit
+                        onClick={isEditBtnClick}
+                    >
+                        <Edit/> Edit
                     </Button>
-
                 </Paper>
             </Grid>
             <Grid item sm={8} xs={12} className={classes.personalInfoBlock2}>
@@ -131,9 +174,7 @@ function usePersonalInfo() {
                             })}
                         </div>
                     </FadeIn>
-
                     :
-
                     <FadeIn>
                         <div className={classes.cards}>
                             {PassagerList && PassagerList.map((el,i)=>{
@@ -147,11 +188,4 @@ function usePersonalInfo() {
     );
 }
 
-function mapStateToProps(state) {
-    return {
-        user: state.user,
-        isLoggedInUser: state.isLoggedInUser,
-        isEdit1:state.isEdit1,
-    };
-}
 export default connect(mapStateToProps)(usePersonalInfo)
